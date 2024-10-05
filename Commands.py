89308abc454ad55d36a7ec2bab2e59e1e63b2aff
Karan Wadhwa <wadhwa.k@northeastern.edu>
2024-10-05 16:54:19 -0500

    feat: hash obj command

--- Commands.py
+++ Commands.py
@@ -1,5 +1,7 @@
+import os
 import sys
 from GitRepository import GitRepository
+from GitObject import GitBlob
 from utils import repo_find
 
 
@@ -13,3 +15,13 @@ def cat_file(args):
     repo = repo_find()
     obj = repo.object_read(args.object)
     sys.stdout.buffer.write(obj.serialize())
+
+
+def hash_obj(args):
+    realpath = os.path.realpath(args.path)
+    with open(realpath, 'rb') as f:
+        data = f.read()
+        blob = GitBlob(data)
+        repo = repo_find()
+        sha = repo.object_write(blob, write=args.write == "True")
+        print(sha)
--- gip.py
+++ gip.py
@@ -6,23 +6,49 @@ def main():
     parser = argparse.ArgumentParser(
         description='wannabe git written in python')
     subparser = parser.add_subparsers(
-        title='Commands', dest='command', required=True, help='use a command')
+        title='Commands',
+        dest='command',
+        required=True,
+        help='use a command')
 
     # init command
     init_parser = subparser.add_parser(
-        'init', help='Create an empty git repository or reinitialize an existing one')
-    init_parser.add_argument("path", metavar="directory", default=".", nargs="?",
+        'init',
+        help='Create an empty git repository or reinitialize an existing one')
+    init_parser.add_argument("path",
+                             metavar="directory",
+                             default=".",
+                             nargs="?",
                              help="Directory path to create the git repository")
     init_parser.set_defaults(func=Commands.init)
 
     # cat-file command
     cat_file_parser = subparser.add_parser(
-        'cat-file', help="Provide content or type and size information for repository objects")
-    cat_file_parser.add_argument("type", metavar="type", choices=[
-                                 "blob", "commit", "tag", "tree"], help="Specify the object type")
+        'cat-file',
+        help="Provide content or type and size information for repository objects")
+    cat_file_parser.add_argument("type",
+                                 metavar="type",
+                                 choices=["blob", "commit", "tag", "tree"],
+                                 help="Specify the object type")
     cat_file_parser.add_argument(
-        "object", metavar="object", help="sha1 hash of the object to be displayed")
+        "object",
+        metavar="object",
+        help="sha1 hash of the object to be displayed")
     cat_file_parser.set_defaults(func=Commands.cat_file)
+
+    # hash-file command
+    hash_file_parser = subparser.add_parser(
+        'hash-object',
+        help="Compute object ID and optionally creates a blob from a file"
+    )
+    hash_file_parser.add_argument("-w",
+                                  dest="write",
+                                  action="store_true",
+                                  default="False",
+                                  help="Actually write the object into the object database")
+    hash_file_parser.add_argument("path",
+                                  help="Hash object as it were located at the given path")
+    hash_file_parser.set_defaults(func=Commands.hash_obj)
 
     args = parser.parse_args()
     args.func(args)
